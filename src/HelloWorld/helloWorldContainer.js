fa60210f1d31b493ac0bf4f896ad750e12db2df2
Nabeel <nabeel.munir@magebit.com>
2022-06-03 11:11:00 +0300

    working with PWA studio section task 6

--- src/HelloWorld/helloWorldContainer.js
+++ src/HelloWorld/helloWorldContainer.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react'
 import TestContext from '../context/testContext';
+import classes from './helloWorldContainer.module.css';
 
 const HelloWorld = () => {
     const { username, setUsernameState } = useContext(TestContext);
     setUsernameState('Nabeel in Magebit')
     
   return (
-      <>
+      <div className={classes.helloWorld}>
         {!username && <span>Hello world!, {HELLO_WORLD}</span>}
         {username && <span>Hello, {username}</span>}
-      </>
+      </div>
   )
 }
 
